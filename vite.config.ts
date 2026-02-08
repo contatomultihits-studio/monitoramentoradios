a76f7055c2f6a9fd6b8e2da8d48adaabc3690aca
contatomultihits-studio <contatomultihits@gmail.com>
2026-02-08 17:33:12 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,18 @@
 import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: { port: 3000, host: '0.0.0.0' },
-      plugins: [react()],
-      build: {
-        rollupOptions: {
-          external: ['react', 'react-dom', 'react-dom/client', 'lucide-react', 'recharts', 'jspdf'],
-        },
-      },
-      resolve: {
-        alias: { '@': path.resolve(__dirname, '.') },
-      }
-    };
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      // ISSO AQUI EVITA O ERRO DE USEREF E O SITE FECHAR:
+      external: ['react', 'react-dom', 'react-dom/client', 'lucide-react', 'recharts', 'jspdf'],
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '.'),
+    },
+  },
 });
